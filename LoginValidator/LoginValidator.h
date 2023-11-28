dca74c43effccb1d8005fb5d7ae3a79e0391a4c8
Glasswala Vinen <vinen.g@elaunchinfotech.in>
2023-11-28 12:35:31 +0530

    first commit

---
+++ LoginValidator/LoginValidator.h
@@ -0,0 +1,18 @@
+//
+//  LoginValidator.h
+//  LoginValidator
+//
+//  Created by iMac on 28/11/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for LoginValidator.
+FOUNDATION_EXPORT double LoginValidatorVersionNumber;
+
+//! Project version string for LoginValidator.
+FOUNDATION_EXPORT const unsigned char LoginValidatorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LoginValidator/PublicHeader.h>
+
+
